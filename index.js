282c07e0409d066a4e096e6035cfddf0a023379a
Gideon Akunana <gideonakunana@gmail.com>
2021-01-29 14:38:05 +0100

    added filter on special price section

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // Banner Owl Carousel
     $('#banner-area .owl-carousel').owlCarousel({
@@ -22,20 +22,19 @@ $(document).ready(function(){
                 items: 5
             }
         }
-    })
+    });
 
     // Isotope Filter
     var $grid = $('.grid').isotope({
         itemSelector: '.grid-item',
         layoutMode: 'fitRows'
-    })
+    });
 
     // Special Products Filter on button click
-    $('.button-group').on('click', 'button', function(){
+    $('.btn-group').on('click', 'button', function () {
         // When a button is clicked. return the data filter attribute
         var filterValue = $(this).attr('data-filter');
-        $grid.isotope({
-            filter: filterValue
-        })
-    })
+        // Pass filter value into isotope object
+        $grid.isotope({ filter: filterValue });
+    });
 });
