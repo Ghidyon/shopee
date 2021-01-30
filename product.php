42103154f68872aac3fe6cfc72312354317297ba
Gideon Akunana <gideonakunana@gmail.com>
2021-01-30 18:07:34 +0100

    converted to product page to .php

---
+++ product.php
@@ -0,0 +1,13 @@
+<?php
+    // include header area
+    require_once('includes/header.php');
+
+    // include banner-area
+    require_once('includes/_product.php');
+
+    // include top-sale area
+    require_once('includes/_top-sale.php');
+
+    // include footer area
+    require_once('includes/footer.php');
+?>
