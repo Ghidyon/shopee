3f32f934bb0c5458a62e8279fe97f8b0bf2e8aca
Gideon Akunana <gideonakunana@gmail.com>
2021-02-03 20:58:39 +0100

    updated alternative data on error

--- includes/_top-sale.php
+++ includes/_top-sale.php
@@ -19,9 +19,9 @@ $product_shuffle = $product->getData();
             <?php foreach ( $product_shuffle as $item ) : ?>
             <div class="item py-2">
                 <div class="product font-raleway">
-                    <a href="#"><img src="<?= $item['item_image'] ?>" class="img-fluid" alt="<?= $item['item_brand'] ?>"></a>
+                    <a href="#"><img src="<?= $item['item_image'] ?? './assets/products/1.png'; ?>" class="img-fluid" alt="<?= $item['item_brand'] ?? 'Unknown'; ?>"></a>
                     <div class="text-center">
-                        <h6><?= $item['item_name'] ?></h6>
+                        <h6><?= $item['item_name'] ?? 'Unknown'; ?></h6>
                         <div class="rating text-warning font-size-12">
                             <span><i class="fas fa-star"></i></span>
                             <span><i class="fas fa-star"></i></span>
@@ -30,7 +30,7 @@ $product_shuffle = $product->getData();
                             <span><i class="far fa-star"></i></span>
                         </div>
                         <div class="price py-2">
-                            <span>$<?= $item['item_price'] ?></span>
+                            <span>$<?= $item['item_price'] ?? '0'; ?></span>
                         </div>
                         <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
                     </div>
