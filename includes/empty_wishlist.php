57411631a9c4abcb0d00bcb9739c1a5f6d0e81c5
Gideon Akunana <gideonakunana@gmail.com>
2021-02-12 10:46:14 +0100

    added empty_wishlist.php

---
+++ includes/empty_wishlist.php
@@ -0,0 +1,21 @@
+<!-- Wishlist Section -->
+<section id="cart">
+    <div class="container pt-2 pb-5">
+        <h4 class="font-baloo font-size-20">Wishlist</h4>
+
+        <!-- Wishlist -->
+        <div class="row border-top py-3 mt-3">
+
+            <!-- Wishlist Empty -->
+            <div class="col-sm-12">
+                <img src="./assets/blog/empty_cart.png" alt="Empty Wishlist" class="img-fluid" style="height:300px">
+                <p class="font-baloo text-black-50 font-size-16">Empty Wishlist</p>
+            </div>
+            <!-- Wishlist Empty Ends -->
+
+        </div>
+        <!-- Wishlist Ends -->
+    </div>
+
+</section>
+<!-- Wishlist Section Ends -->
