40203153fa0e7159719709f24944984eb1542d54
Gideon Akunana <gideonakunana@gmail.com>
2021-02-12 11:01:50 +0100

    updated img height

--- includes/empty_cart.php
+++ includes/empty_cart.php
@@ -6,7 +6,7 @@
         <!-- Empty Cart -->
         <div class="row border-top py-3 mt-3">
             <div class="col-sm-12 text-center">
-                <img src="./assets/blog/empty_cart.png" alt="Empty Cart" class="img-fluid" style="height:300px">
+                <img src="./assets/blog/empty_cart.png" alt="Empty Cart" class="img-fluid" style="height:200px">
                 <p class="font-baloo text-black-50 font-size-16">Empty Cart</p>
             </div>
         </div>
--- includes/empty_wishlist.php
+++ includes/empty_wishlist.php
@@ -7,8 +7,8 @@
         <div class="row border-top py-3 mt-3">
 
             <!-- Wishlist Empty -->
-            <div class="col-sm-12">
-                <img src="./assets/blog/empty_cart.png" alt="Empty Wishlist" class="img-fluid" style="height:300px">
+            <div class="col-sm-12 text-center">
+                <img src="./assets/blog/empty_cart.png" alt="Empty Wishlist" class="img-fluid" style="height:200px">
                 <p class="font-baloo text-black-50 font-size-16">Empty Wishlist</p>
             </div>
             <!-- Wishlist Empty Ends -->
